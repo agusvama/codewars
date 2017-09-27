ed7b97fda15bb9523ec05b28e0e693ec8b16a21f
maldad <canond_7@hotmail.com>
2017-09-27 17:34:28 -0500

    Tribonacci in haskell

--- haskell/Tribonacci/Tribonacci.hs
+++ haskell/Tribonacci/Tribonacci.hs
@@ -4,8 +4,9 @@ last3 xs = [last xs] ++ [last(init xs)] ++ [last(init(init xs))]
 sumLast3 xs = sum(last3 xs)
 
 tribonacci :: Num a => (a, a, a) -> Int -> [a]
-tribonacci (a, b, c) n = recursive 3 n [a, b, c]
-  where recursive start end list
-          | start == end = list
-          | end == 0 = []
-          | otherwise = recursive (succ start) n (list ++ [sumLast3 list])
+tribonacci (a, b, c) 0 = []
+tribonacci (a, b, c) n = take n (recursive n [a, b, c])
+
+recursive n list
+  | n == 0 = list
+  | otherwise = recursive (pred n) (list ++ [sumLast3 list])
